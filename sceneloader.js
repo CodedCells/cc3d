e239107a8abfd3516c2e11c33f3b225352b0d8bf
Ethan <codedcells@gmail.com>
2023-02-03 21:44:30 +0000

    added true freecam
    toggle cameras with space key, press r to reset position

--- sceneloader.js
+++ sceneloader.js
@@ -10,6 +10,9 @@ var sphere;
 var mixer;
 var clips;
 
+var cameraPos = new THREE.Vector3();
+var cameraRot = new THREE.Vector3();
+
 var lastTime;
 
 let forward = new THREE.Vector3();
@@ -39,9 +42,6 @@ const clickEvent = (event) => {
 };
 
 let lookX = 0, lookY = 0;
-let mouseX = 0, mouseY = 0;
-let velocity = new THREE.Vector3();
-
 function onDocumentMouseMove(event) {
 	if (isLocked) {
 		mouseX = event.movementX || event.mozMovementX || event.webkitMovementX || 0;
@@ -50,34 +50,39 @@ function onDocumentMouseMove(event) {
 		lookX -= mouseX * 0.002;
 		lookY -= mouseY * 0.002;
 		
-		fpscamera.lookAt(new THREE.Vector3(
-			fpscamera.position.x + Math.sin(lookX),
-			fpscamera.position.y + lookY,
-			fpscamera.position.z + Math.cos(lookX)
+		activecam.lookAt(new THREE.Vector3(
+			activecam.position.x + Math.sin(lookX),
+			activecam.position.y + lookY,
+			activecam.position.z + Math.cos(lookX)
 		));
 	}
 }
 
 document.addEventListener('mousemove', onDocumentMouseMove, false);
 
+var heldKeys = {"KeyW": false, "KeyA": false, "KeyS": false, "KeyD": false};
+var freecam = false;
+var activecam = camera;
+
 function onDocumentKeyDown(event) {
-	switch (event.code) {
-		case 'KeyW':
-			velocity.z = -0.05;
-			break;
-		case 'KeyS':
-			velocity.z = 0.05;
-			break;
-		case 'KeyA':
-			velocity.x = -0.05;
-			break;
-		case 'KeyD':
-			velocity.x = 0.05;
-			break;
+	heldKeys[event.code] = true;
+	if (event.code == "KeyR") {
+		activecam.position.copy(cameraPos);
+		activecam.quaternion.copy(cameraRot);
+	}
+	if (event.code == "Space") {
+		freecam = !(freecam);
+		if (freecam) activecam = fpscamera;
+		else activecam = camera;
 	}
 }
 
+function onDocumentKeyUp(event) {
+	heldKeys[event.code] = false;
+}
+
 document.addEventListener('keydown', onDocumentKeyDown, false);
+document.addEventListener('keyup', onDocumentKeyUp, false);
 
 function animate(now) {
 	requestAnimationFrame( animate );
@@ -87,11 +92,13 @@ function animate(now) {
 	var elapsed = now - lastTime;
 	lastTime = now;
 	
-	fpscamera.position.x += velocity.x;
-	fpscamera.position.z += velocity.z;
-	velocity.set(0, 0, 0);
-  
-	if (ready) renderer.render( scene, fpscamera );
+	forward.set(0, 0, -1).applyQuaternion(activecam.quaternion);
+	if (heldKeys.KeyW) activecam.position.add(forward.multiplyScalar(0.1));
+	if (heldKeys.KeyS) activecam.position.sub(forward.multiplyScalar(0.1));
+	if (heldKeys.KeyA) activecam.position.sub(forward.cross(camera.up).normalize().multiplyScalar(0.1));
+	if (heldKeys.KeyD) activecam.position.add(forward.cross(camera.up).normalize().multiplyScalar(0.1));
+
+	if (ready) renderer.render( scene, activecam );
 	
 	if (mixer) mixer.update(elapsed / 1000);
 };
@@ -111,6 +118,7 @@ function initial() {
 function basicScene() {
 	scene = new THREE.Scene();
 	camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+	activecam = camera;
 
 	renderer = new THREE.WebGLRenderer({ antialias: true });
 	renderer.setSize( window.innerWidth, window.innerHeight );
@@ -131,9 +139,9 @@ document.addEventListener('mozpointerlockchange', lockChangeAlert, false);
 document.addEventListener('webkitpointerlockchange', lockChangeAlert, false);
 
 function onDocumentMouseWheel(event) {
-	fpscamera.fov += event.deltaY * 0.01;
-	fpscamera.fov = Math.min(Math.max(fpscamera.fov, 1), 179)
-	fpscamera.updateProjectionMatrix();
+	activecam.fov += event.deltaY * 0.01;
+	activecam.fov = Math.min(Math.max(activecam.fov, 1), 179)
+	activecam.updateProjectionMatrix();
 }
 
 document.addEventListener("wheel", onDocumentMouseWheel, false);
@@ -145,6 +153,7 @@ function importScene(data) {
 	sceneData = data;
 	scene = new THREE.ObjectLoader().parse( sceneData );
 	camera = scene.children[0].children.find( child => child instanceof THREE.PerspectiveCamera );
+	activecam = camera;
 	
 	mixer = new THREE.AnimationMixer(scene.children[0]);
 	clips = scene.children[0].animations;
@@ -154,14 +163,17 @@ function importScene(data) {
 	clips.forEach( function ( clip ) {
 		mixer.clipAction( clip ).play();
 	} );
+	cameraPos.copy(camera.position);
+	cameraRot.copy(camera.quaternion);
 	fpscamera.position.copy(camera.position);
 	fpscamera.quaternion.copy(camera.quaternion);
-	fpscamera.fov = camera.fov;
-	lookX = camera.rotation.x;
-	lookY = camera.rotation.y;
+	
+	let cameraEuler = new THREE.Euler().setFromQuaternion(fpscamera.quaternion, 'XYZ');
+	
+	lookX = -cameraEuler.x * 360;
+	lookY = -cameraEuler.y;
 	
 	scene.add(fpscamera);
-	//scene.add(sphere);
 	
 	//let ambientLight = new THREE.AmbientLight(0xB1BEDD);
 	//scene.add(ambientLight);
