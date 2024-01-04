b14d01bc26fba4113727b9d71526e38ee65f0ad4
Ethan <codedcells@gmail.com>
2024-01-04 20:41:29 +0000

    updated menu navigation
    can now order by date, title and type

---
+++ new/core.js
@@ -0,0 +1,114 @@
+import * as THREE from 'three';
+
+import { OrbitControls } from './rip/OrbitControls.js';
+import { GLTFLoader } from './rip/GLTFLoader.js';
+import { RGBELoader } from './rip/RGBELoader.js';
+
+export let camera, scene, renderer, model, animations, mixer, orbitObject;
+var lastTime;
+export var playSpeed = 1;
+
+export function initialiseDefaultScene(c) {
+	renderer = new THREE.WebGLRenderer( { antialias: true } );
+	renderer.setPixelRatio( window.devicePixelRatio );
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer.toneMapping = THREE.LinearToneMapping ;
+	renderer.toneMappingExposure = 1;
+	c.appendChild( renderer.domElement );
+}
+
+export function loadScene(fn) {
+	camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.25, 500 );
+
+	scene = new THREE.Scene();
+	
+	const controls = new OrbitControls( camera, renderer.domElement );
+	controls.addEventListener( 'change', render ); // use if there is no animation loop
+	controls.minDistance = 0;
+	controls.maxDistance = 30;
+	
+	// model
+	const loader = new GLTFLoader().setPath( 'scenes/' );
+	loader.load( fn, function ( gltf ) {
+		
+		// this magical placement of the function
+		animate();
+		// makes it all work
+		// all hail chatgpt debugging
+		// do not move it lest ye turn to stone
+		
+		model = gltf.scene;
+		
+		model.traverse((obj) => {
+			if (obj.isMesh) {
+				obj.renderOrder = 0; // Set render order
+				obj.material.depthWrite = true; // Enable depth writing for the material
+				obj.material.alphaTest = 0.05;
+				
+				if (obj.material.map)
+					obj.material.map.minFilter = THREE.LinearFilter;
+			}
+		});
+		
+		var orbitObject = model.getObjectByName("@orbit");
+		if (orbitObject) {
+			if (orbitObject.userData.ambientLight) {
+				var ambientLightColor = new THREE.Color("#" + orbitObject.userData.ambientLight);
+				var ambientLight = new THREE.AmbientLight(ambientLightColor, 0.8);
+				scene.add(ambientLight);
+			}
+			if (orbitObject.userData.sky) {
+				var skyTextureName = "skies/" + orbitObject.userData.sky + ".jpg";
+				var textureLoader = new THREE.TextureLoader();
+				var skyTexture = textureLoader.load(skyTextureName);
+				skyTexture.colorSpace = THREE.SRGBColorSpace;
+				skyTexture.mapping = THREE.EquirectangularReflectionMapping;
+
+				scene.background = skyTexture;
+			}
+		
+			orbitObject = orbitObject.position;
+			controls.target.set( orbitObject.x, orbitObject.y, orbitObject.z );
+		}
+		else
+			controls.target.set( 0, 1.6, 0 );
+		
+		controls.update();
+		
+		animations = gltf.animations;
+		mixer = new THREE.AnimationMixer(model); // Assuming 'model' is your GLTF scene
+		
+		animations.forEach((clip) => {
+			mixer.clipAction(clip).play();
+		});
+		
+		mixer.update(0);
+		
+		const gltfCamera = gltf.cameras[0];
+		camera.position.copy(gltfCamera.position);
+		camera.rotation.copy(gltfCamera.rotation);
+		camera.fov = gltfCamera.fov;
+		camera.updateProjectionMatrix();
+		
+		// wait until the model can be added to the scene without blocking due to shader compilation
+		scene.add( model );
+	} );
+}
+
+function animate(now) {
+	requestAnimationFrame(animate);
+	if (!lastTime) { lastTime = now; }
+	
+	var elapsed = now - lastTime;
+	lastTime = now;
+	
+	if (mixer && playSpeed > 0)
+		mixer.update(elapsed / 1000 * playSpeed); // 'deltaTime' is the time difference between frames
+	
+	render();
+}
+
+export function render() {
+	if (camera)
+		renderer.render( scene, camera );
+}
---
+++ new/menu.js
@@ -0,0 +1,212 @@
+import * as core from './core.js';
+
+var fails = 0;
+var scenes = {};
+const linkDisp = document.createElement('span');
+
+function reload() {
+	// Reload the page
+	var query = this.id;
+	window.location.hash = "";
+	if (query)
+		query = "?s=" + query;
+	
+	window.location.search = query;
+}
+
+function onWindowResize() {
+
+	if (core.camera) {
+		core.camera.aspect = window.innerWidth / window.innerHeight;
+		core.camera.updateProjectionMatrix();
+	}
+	core.renderer.setSize( window.innerWidth, window.innerHeight );
+
+	core.render();
+}
+
+function presentBack() {
+	const sceneMenu = document.getElementById("sceneMenu");
+	const sceneSelectDiv = document.createElement('div');
+	sceneMenu.className = "menuBack";
+	
+	const optVis = document.createElement('div');
+	
+	optVis.className = "menuOption";
+	optVis.id = "";
+	optVis.onclick = reload;
+	
+	const optVisTitle = document.createElement('span');
+	optVisTitle.innerHTML = "< Back";
+	optVis.appendChild(optVisTitle);
+	sceneSelectDiv.appendChild(optVis);
+	
+	sceneMenu.appendChild(sceneSelectDiv);
+}
+
+function init() {
+
+	const container = document.createElement( 'div' );
+	document.body.appendChild( container );
+	
+	core.initialiseDefaultScene(container);
+	
+	var loadThis;
+	
+	if (window.location.search) {
+		loadThis = window.location.search.substr(3);
+		presentBack()
+	}
+	else if (window.location.hash) {
+		loadThis = window.location.hash.substr(1);
+		presentBack()
+	} 
+	
+	if (!loadThis) {
+		loadThis = 'spin'
+		fetchScenes()
+	}
+	
+	core.loadScene(loadThis + '.glb')
+	
+	window.addEventListener( 'resize', onWindowResize );
+}
+
+function fetchScenes() {
+	fetch('scenes.json')
+	.then(response => response.json()).catch(error => {
+		console.error('Error fetching JSON:', error);
+		if (fails++ < 6) {
+			const x = setTimeout(fetchScenes, 500);
+		} else {
+			presentFail();
+		}
+	})
+	.then(data => {
+		// Call a function or do something with the JSON data
+		scenes = data;
+		presentLinks();
+	});
+}
+
+function presentLink(sid, info) {
+	const optVis = document.createElement('div');
+	
+	optVis.className = "menuOption";
+	optVis.id = sid;
+	optVis.onclick = reload;
+	
+	const optVisTitle = document.createElement('span');
+	
+	var type = info.type;
+	if (!type) type = "static";
+	type = type.toLowerCase();
+	
+	optVisTitle.innerHTML += `<span class="mode-icon mode-icon-${type}"></span> `;
+	
+	optVisTitle.innerHTML += info.title;
+	
+	optVis.appendChild(optVisTitle);
+	return optVis;
+}
+
+function sort_date(data) {
+	const entries = Object.entries(data);
+
+	// Sort the array based on the date property
+	entries.sort((a, b) => b[1].date - a[1].date);
+	
+	return entries.map(entry => entry[0]);
+}
+
+function sort_title(data) {
+	const entries = Object.entries(data);
+
+	// Sort the array based on the date property
+	entries.sort((a, b) => a[1].title.toUpperCase() - b[1].title.toUpperCase());
+	
+	return entries.map(entry => entry[0]);
+}
+
+function sort_type(data) {
+	const entries = Object.entries(data);
+
+	// Sort the array based on the date property
+	entries.sort((a, b) => a[1].type - b[1].type);
+	
+	return entries.map(entry => entry[0]);
+}
+
+function presentLinks() {
+	const sceneMenu = document.getElementById("sceneMenu");
+	const sceneSelectDiv = document.createElement('div');
+	sceneMenu.className = "menuMain";
+	
+	const divTitle = document.createElement('h2');
+	divTitle.innerHTML = "Scene Loader";
+	sceneSelectDiv.appendChild(divTitle);
+	sceneSelectDiv.appendChild(linkDisp);
+	sceneMenu.appendChild(sceneSelectDiv);
+	
+	presentOrderedLinks();
+}
+
+function sortScenesTrigger() {
+	presentOrderedLinks(this.id);
+}
+
+function presentOrderedLinks(order) {
+	linkDisp.innerHTML = "";
+	
+	const orderings = {
+		"date": sort_date,
+		"title": sort_title,
+		"type": sort_type,
+	}
+	
+	var ascending = false;
+	if (!order) order = "date";
+	else if (order.endsWith("_asc")) {
+		ascending = true;
+		order = order.substring(0, order.length - 4);
+	}
+	
+	if (!order in orderings) {
+		console.log(`Ordering "${order}" not supported, defaulting to name.`)
+		order = "name";
+	}
+	
+	var rx = 0;
+	for (var [name] of Object.entries(orderings)) {
+		var optVis = document.createElement('div');
+		optVis.className = "orderOption";
+		if (order == name && !ascending)
+			optVis.className += " orderSelected";
+		
+		optVis.id = name;
+		optVis.style.backgroundPositionX = `-${48 * rx++}px`;
+		optVis.onclick = sortScenesTrigger;
+		linkDisp.appendChild(optVis);
+		
+		var optVis = document.createElement('div');
+		optVis.className = "orderOption";
+		if (order == name && ascending)
+			optVis.className += " orderSelected";
+		
+		optVis.id = name+"_asc";
+		optVis.style.backgroundPositionX = `-${48 * rx++}px`;
+		optVis.onclick = sortScenesTrigger;
+		linkDisp.appendChild(optVis);
+	}
+	
+	order = orderings[order](scenes);
+	
+	if (ascending) order = order.reverse();
+	
+	for (var [i, sid] of Object.entries(order)) {
+		const vis = presentLink(sid, scenes[sid]);
+		linkDisp.appendChild(vis);
+	}
+}
+
+init();
