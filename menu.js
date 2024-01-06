38a2098db65b315555e1b66c5f59e043f09e4e0a
Ethan <codedcells@gmail.com>
2024-01-06 00:00:24 +0000

    yeah
    don't do that

--- menu.js
+++ menu.js
@@ -52,13 +52,14 @@ function presentBack(sid) {
 	const info = scenes[sid];
 	optVisInfo.innerHTML = `<h3>${info.title}</h3>`;
 	
-	if (info.post_ids)
+	if (info.post_ids) {
 		optVisInfo.innerHTML += "FA: ";
 	
-	for (var i = 0; i < info.post_ids.length; i++) {
-		var d = info.post_ids[i];
-		if (i > 0) optVisInfo.innerHTML += ", ";
-		optVisInfo.innerHTML += `<a href="https://furaffinity.net/view/${d}/">${d}</a>`;
+		for (var i = 0; i < info.post_ids.length; i++) {
+			var d = info.post_ids[i];
+			if (i > 0) optVisInfo.innerHTML += ", ";
+			optVisInfo.innerHTML += `<a href="https://furaffinity.net/view/${d}/">${d}</a>`;
+		}
 	}
 	sceneSelectDiv.appendChild(optVisInfo);
 	
