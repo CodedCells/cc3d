fe7a1b0066c25322fc9d2e807b0d3649cc6e41d2
Ethan <codedcells@gmail.com>
2024-08-31 14:31:33 +0100

    code tidyup
    wanna make it less ugly

--- core.js
+++ core.js
@@ -1,8 +1,8 @@
 import * as THREE from 'three';
 
-import { OrbitControls } from './rip/OrbitControls.js';
-import { GLTFLoader } from './rip/GLTFLoader.js';
-import { RGBELoader } from './rip/RGBELoader.js';
+import {OrbitControls} from './rip/OrbitControls.js';
+import {GLTFLoader} from './rip/GLTFLoader.js';
+import {RGBELoader} from './rip/RGBELoader.js';
 import Stats from './rip/stats.module.js';
 
 export let sceneReady, camera, controls, scene, renderer, model, animations, mixer, orbitObject, stats;
@@ -13,23 +13,97 @@ sceneReady = false;
 var applicationReadyFlag = true;
 
 export function setPlaySpeed(value) {
-    playSpeed = value;
+	playSpeed = value;
 }
 export function applicationReady(value) {
-    applicationReadyFlag = value;
+	applicationReadyFlag = value;
 }
 
 export function setPixelRatio(v) {
 	renderer.setPixelRatio(v);
 }
 
-export function initialiseDefaultScene(c) {	
-	renderer = new THREE.WebGLRenderer( { antialias: true } );
-	renderer.setPixelRatio( window.devicePixelRatio );
-	renderer.setSize( window.innerWidth, window.innerHeight );
-	renderer.toneMapping = THREE.LinearToneMapping ;
+export function initialiseDefaultScene(c) {
+	renderer = new THREE.WebGLRenderer({
+		antialias: true
+	});
+	renderer.setPixelRatio(window.devicePixelRatio);
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.toneMapping = THREE.LinearToneMapping;
 	renderer.toneMappingExposure = 1;
-	c.appendChild( renderer.domElement );
+	c.appendChild(renderer.domElement);
+}
+
+function loaderReady(gltf) {
+	// this magical placement of the function
+	animate();
+	// makes it all work
+	// all hail chatgpt debugging
+	// do not move it lest ye turn to stone
+
+	model = gltf.scene;
+
+	model.traverse((obj) => {
+		if (obj.isMesh) {
+			obj.renderOrder = 0; // Set render order
+			obj.material.depthWrite = true; // Enable depth writing for the material
+			obj.material.alphaTest = 0.05;
+
+			if (obj.material.map)
+				obj.material.map.minFilter = THREE.LinearFilter;
+		}
+	});
+
+	var orbitObject = model.getObjectByName("@orbit");
+	if (orbitObject) {
+
+		var ambient = orbitObject.userData.ambientLight;
+		if (!ambient)
+			ambient = "404047";
+
+		var ambientIntensity = orbitObject.userData.ambientIntensity;
+		if (!ambientIntensity)
+			ambientIntensity = 0.8;
+
+		var ambientLightColor = new THREE.Color("#" + ambient);
+		var ambientLight = new THREE.AmbientLight(ambientLightColor, ambientIntensity);
+		scene.add(ambientLight);
+
+		if (orbitObject.userData.sky) {
+			var skyTextureName = "/cc3d/skies/" + orbitObject.userData.sky + ".jpg";
+			var textureLoader = new THREE.TextureLoader();
+			var skyTexture = textureLoader.load(skyTextureName);
+			skyTexture.colorSpace = THREE.SRGBColorSpace;
+			skyTexture.mapping = THREE.EquirectangularReflectionMapping;
+
+			scene.background = skyTexture;
+		}
+
+		orbitObject = orbitObject.position;
+		controls.target.set(orbitObject.x, orbitObject.y, orbitObject.z);
+	} else
+		controls.target.set(0, 1.6, 0);
+
+	controls.update();
+
+	animations = gltf.animations;
+	mixer = new THREE.AnimationMixer(model); // Assuming 'model' is your GLTF scene
+
+	animations.forEach((clip) => {
+		mixer.clipAction(clip).play();
+	});
+
+	mixer.update(0);
+
+	const gltfCamera = gltf.cameras[0];
+	camera.position.copy(gltfCamera.position);
+	camera.rotation.copy(gltfCamera.rotation);
+	camera.fov = gltfCamera.fov;
+	camera.updateProjectionMatrix();
+
+	// wait until the model can be added to the scene without blocking due to shader compilation
+	scene.add(model);
+	sceneReady = true;
 }
 
 export function loadScene(fn, path, hash) {
@@ -42,144 +116,72 @@ export function loadScene(fn, path, hash) {
 	}
 	if (hash === undefined)
 		hash = Math.random()
-	console.log(hash);
-	
+
 	sceneFile = fn;
-	if (!camera)
-		camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.25, 500 );
 	
+	if (!camera)
+		camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.25, 500);
+
 	if (scene)
 		scene.remove.apply(scene, scene.children);
-	
+
 	scene = new THREE.Scene();
-	
+
 	if (controls)
 		controls.dispose();
-	
-	controls = new OrbitControls( camera, renderer.domElement );
-	controls.addEventListener( 'change', render ); // use if there is no animation loop
+
+	controls = new OrbitControls(camera, renderer.domElement);
+	controls.addEventListener('change', render); // use if there is no animation loop
 	controls.minDistance = 0;
 	controls.maxDistance = 30;
-	
+
 	if (path === undefined)
 		path = '/cc3d/scenes/';
+	
 	// model
-	const loader = new GLTFLoader().setPath( path );
-	loader.load(fn + ".glb?v="+hash, function ( gltf ) {
-		
-		// this magical placement of the function
-		animate();
-		// makes it all work
-		// all hail chatgpt debugging
-		// do not move it lest ye turn to stone
-		
-		model = gltf.scene;
-		
-		model.traverse((obj) => {
-			if (obj.isMesh) {
-				obj.renderOrder = 0; // Set render order
-				obj.material.depthWrite = true; // Enable depth writing for the material
-				obj.material.alphaTest = 0.05;
-				
-				if (obj.material.map)
-					obj.material.map.minFilter = THREE.LinearFilter;
-			}
-		});
-		
-		var orbitObject = model.getObjectByName("@orbit");
-		if (orbitObject) {
-			
-			var ambient = orbitObject.userData.ambientLight;
-			if (!ambient)
-				ambient = "404047";
-			
-			var ambientIntensity = orbitObject.userData.ambientIntensity;
-			if (!ambientIntensity)
-				ambientIntensity = 0.8;
-			
-			var ambientLightColor = new THREE.Color("#" + ambient);
-			var ambientLight = new THREE.AmbientLight(ambientLightColor, ambientIntensity);
-			scene.add(ambientLight);
-			
-			if (orbitObject.userData.sky) {
-				var skyTextureName = "/cc3d/skies/" + orbitObject.userData.sky + ".jpg";
-				var textureLoader = new THREE.TextureLoader();
-				var skyTexture = textureLoader.load(skyTextureName);
-				skyTexture.colorSpace = THREE.SRGBColorSpace;
-				skyTexture.mapping = THREE.EquirectangularReflectionMapping;
-
-				scene.background = skyTexture;
-			}
-		
-			orbitObject = orbitObject.position;
-			controls.target.set( orbitObject.x, orbitObject.y, orbitObject.z );
-		}
-		else
-			controls.target.set( 0, 1.6, 0 );
-		
-		controls.update();
-		
-		animations = gltf.animations;
-		mixer = new THREE.AnimationMixer(model); // Assuming 'model' is your GLTF scene
-		
-		animations.forEach((clip) => {
-			mixer.clipAction(clip).play();
-		});
-		
-		mixer.update(0);
-		
-		const gltfCamera = gltf.cameras[0];
-		camera.position.copy(gltfCamera.position);
-		camera.rotation.copy(gltfCamera.rotation);
-		camera.fov = gltfCamera.fov;
-		camera.updateProjectionMatrix();
-		
-		// wait until the model can be added to the scene without blocking due to shader compilation
-		scene.add( model );
-		sceneReady = true;
+	const loader = new GLTFLoader().setPath(path);
+	loader.load(fn + ".glb?v=" + hash, loaderReady,
+	function(xhr) {
+		// This function will be called while the model is loading, you can use it for progress updates if needed
+		//console.log(xhr.loaded, xhr.total);
 	},
-	function (xhr) {
-        // This function will be called while the model is loading, you can use it for progress updates if needed
-        //console.log(xhr.loaded, xhr.total);
-    },
-	function (error) {
-        // Handle errors, including 404 Not Found
+	function(error) {
+		// Handle errors, including 404 Not Found
 		if (fn != 'spin')
 			loadScene("error");
 		
-        if (error.status === 404) {
-            console.error('Model not found: ' + modelUrl);
-            // Handle the 404 error gracefully, e.g., show a placeholder or a message to the user
-        } else {
-            console.error('Error loading model:', error);
-            // Handle other types of errors
-        }
-    }
+		if (error.status === 404)
+			console.error('Model not found: ' + modelUrl);
+		else
+			console.error('Error loading model:', error);
+	}
 	);
 }
 
 function animate(now) {
 	requestAnimationFrame(animate);
-	if (!lastTime) { lastTime = now; }
-	
+	if (!lastTime) {
+		lastTime = now;
+	}
+
 	var elapsed = now - lastTime;
 	lastTime = now;
-	
+
 	if (mixer && playSpeed > 0)
 		mixer.update(elapsed / 1000 * playSpeed); // 'deltaTime' is the time difference between frames
-	
+
 	render();
 }
 
 export function render() {
 	if (!camera) return
-	
+
 	sceneReady = true;
 	if (!applicationReadyFlag) return
-	
+
 	stats.begin();
-	
-	renderer.render( scene, camera );
-	
+
+	renderer.render(scene, camera);
+
 	stats.end();
 }
--- menu.js
+++ menu.js
@@ -9,10 +9,10 @@ const linkDisp = document.createElement('span');
 function reload() {
 	var newState = window.location.pathname;
 	var query = this.id;
-	
+
 	if (query)
 		newState += "?s=" + query;
-	
+
 	window.history.pushState(query, '', newState);
 	getLoadSceneUI()
 }
@@ -20,19 +20,18 @@ function reload() {
 window.addEventListener('popstate', getLoadSceneUI);
 
 function onWindowResize() {
-
 	if (core.camera) {
 		core.camera.aspect = window.innerWidth / window.innerHeight;
 		core.camera.updateProjectionMatrix();
 	}
-	core.renderer.setSize( window.innerWidth, window.innerHeight );
+	core.renderer.setSize(window.innerWidth, window.innerHeight);
 
 	core.render();
 }
 
 function presentIDLinks(disp, source, ids) {
 	var out = disp;
-		
+
 	for (var i = 0; i < ids.length; i++) {
 		var d = ids[i];
 		if (i > 0) out += ", ";
@@ -46,18 +45,18 @@ function presentBackInfo(sid, info) {
 	const pageTitle = info.title + " - CodedCells 3D Scene Viewer";;
 	document.title = pageTitle;
 	document.getElementById("ogTitle").content = pageTitle;
-	
+
 	optVisInfo.innerHTML = `<h3>${info.title}</h3>`;
-	
+
 	if (info.post_ids) {
 		if (info.post_ids.FA && info.post_ids.FA.length)
 			optVisInfo.innerHTML += presentIDLinks("FA:", "https://furaffinity.net/view/", info.post_ids.FA);
 		if (info.post_ids.E6 && info.post_ids.E6.length)
 			optVisInfo.innerHTML += presentIDLinks("E621:", "https://e621.net/posts/", info.post_ids.E6);
 	}
-	
+
 	if (info.post_ids && info.post_ids.FA.length) {
-		
+
 	}
 	return optVisInfo;
 }
@@ -65,25 +64,25 @@ function presentBackInfo(sid, info) {
 function presentBack(sid, abs) {
 	const sceneMenu = document.getElementById("sceneMenu");
 	sceneMenu.innerHTML = "";
-	
+
 	const sceneSelectDiv = document.createElement('div');
 	sceneMenu.className = "menuBack";
-	
+
 	const optVis = document.createElement('div');
-	
+
 	optVis.className = "menuOption";
 	optVis.id = "";
-	
+
 	const info = scenes[sid];
 	if (!info) abs = true;
-	
+
 	if (abs) {
 		// hacky because onclick fucking dies after 404
 		const optVisTitle = document.createElement('a');
 		optVisTitle.innerHTML = "< Back";
 		optVisTitle.href = "/cc3d/sceneloader_nsfw.html";
 		optVis.appendChild(optVisTitle);
-		
+
 		sceneSelectDiv.appendChild(optVis);
 	} else {
 		const optVisTitle = document.createElement('span');
@@ -92,7 +91,7 @@ function presentBack(sid, abs) {
 		optVis.onclick = reload;
 		sceneSelectDiv.appendChild(optVis);
 	}
-	
+
 	if (info) {
 		sceneSelectDiv.appendChild(presentBackInfo(sid, info));
 	} else {
@@ -100,19 +99,18 @@ function presentBack(sid, abs) {
 		if (parseInt(sid) >= 28747249)
 			sceneSelectDiv.innerHTML += "<br>Perhaps wrong post ID?";
 	}
-	
+
 	sceneMenu.appendChild(sceneSelectDiv);
 }
 
 function init() {
+	const container = document.createElement('div');
+	document.body.appendChild(container);
 
-	const container = document.createElement( 'div' );
-	document.body.appendChild( container );
-	
 	core.initialiseDefaultScene(container);
-	
-	window.addEventListener( 'resize', onWindowResize );
-	
+
+	window.addEventListener('resize', onWindowResize);
+
 	fetchScenes();
 }
 
@@ -128,78 +126,76 @@ function reverseSceneIDs() {
 function preparseThis(loadThis) {
 	if (loadThis in sceneIds)
 		loadThis = sceneIds[loadThis];
-	
+
 	return loadThis;
 }
 
 function getLoadSceneUI() {
 	var loadThis;
-	
+
 	if (window.location.search) {
 		loadThis = preparseThis(window.location.search.substr(3));
 		presentBack(loadThis)
-	}
-	else if (window.location.hash) {
+	} else if (window.location.hash) {
 		loadThis = preparseThis(window.location.hash.substr(1));
 		presentBack(loadThis)
-	}
-	else if (window.location.pathname.includes("/s")) {
+	} else if (window.location.pathname.includes("/s")) {
 		loadThis = preparseThis(window.location.pathname.split("/")[3]);
 		presentBack(loadThis, true)
 	}
-	
+
 	if (!loadThis) {
 		if (!core.sceneFile) loadThis = 'spin'
 		presentLinks()
 	}
-	
+
 	if (loadThis && loadThis != core.sceneFile) {
 		//console.log("chaging to", loadThis);
 		let hash;
 		if (scenes[loadThis])
 			hash = scenes[loadThis]['hash'];
-		
+
 		core.loadScene(loadThis, undefined, hash);
 	}
 }
 
 function fetchScenes() {
-	fetch('/cc3d/scenes.json?v='+Math.random())
-	.then(response => response.json()).catch(error => {
-		console.error('Error fetching JSON:', error);
-		if (fails++ < 6) {
-			const x = setTimeout(fetchScenes, 500);
-		} else {
-			presentFail();
-		}
-	})
-	.then(data => {
-		// Call a function or do something with the JSON data
-		scenes = data;
-		reverseSceneIDs();
-		getLoadSceneUI();
-	});
+	fetch('/cc3d/scenes.json?v=' + Math.random())
+		.then(response => response.json()).catch(error => {
+			console.error('Error fetching JSON:', error);
+			if (fails++ < 6) {
+				const x = setTimeout(fetchScenes, 500);
+			}
+			else
+				presentFail();
+		})
+		.then(data => {
+			// Call a function or do something with the JSON data
+			scenes = data;
+			reverseSceneIDs();
+			getLoadSceneUI();
+		});
 }
 
 function presentLink(sid, info, abs) {
 	const optVis = document.createElement('a');
-	
+
 	optVis.className = "menuOption";
 	optVis.id = sid;
-	
+
 	if (abs) optVis.href = `/cc3d/s/${sid}/`;
 	else optVis.onclick = reload;
-	
+
 	const optVisTitle = document.createElement('span');
-	
+
 	var type = info.type;
 	if (!type) type = "static";
 	type = type.toLowerCase();
-	
+
 	optVisTitle.innerHTML += `<span class="mode-icon mode-icon-${type}"></span> `;
-	
+
 	optVisTitle.innerHTML += info.title;
-	
+
 	optVis.appendChild(optVisTitle);
 	return optVis;
 }
@@ -229,13 +225,13 @@ function presentLinks() {
 	sceneMenu.innerHTML = "";
 	const sceneSelectDiv = document.createElement('div');
 	sceneMenu.className = "menuMain";
-	
+
 	const divTitle = document.createElement('h2');
 	divTitle.innerHTML = "Scene Loader";
 	sceneSelectDiv.appendChild(divTitle);
 	sceneSelectDiv.appendChild(linkDisp);
 	sceneMenu.appendChild(sceneSelectDiv);
-	
+
 	presentOrderedLinks();
 }
 
@@ -245,75 +241,75 @@ function sortScenesTrigger() {
 
 function presentOrderedLinks(order) {
 	linkDisp.innerHTML = "";
-	
+
 	if (!order) order = storedOrder;
 	else storedOrder = order;
-	
+
 	const orderings = {
 		"date": sort_date,
 		"title": sort_title,
 		"type": sort_type,
 	}
-	
+
 	var ascending = false;
 	if (!order) order = "date";
 	else if (order.endsWith("_asc")) {
 		ascending = true;
 		order = order.substring(0, order.length - 4);
 	}
-	
+
 	if (!order in orderings) {
 		console.log(`Ordering "${order}" not supported, defaulting to name.`)
 		order = "name";
 	}
-	
+
 	const abs = window.location.pathname.includes("sceneloader_nsfw.html");
-	
+
 	var rx = 0;
 	for (var [name] of Object.entries(orderings)) {
 		var optVis = document.createElement('div');
 		optVis.className = "orderOption";
 		if (order == name && !ascending)
 			optVis.className += " orderSelected";
-		
+
 		optVis.id = name;
 		optVis.style.backgroundPositionX = `-${48 * rx++}px`;
 		optVis.onclick = sortScenesTrigger;
 		linkDisp.appendChild(optVis);
-		
+
 		var optVis = document.createElement('div');
 		optVis.className = "orderOption";
 		if (order == name && ascending)
 			optVis.className += " orderSelected";
-		
-		optVis.id = name+"_asc";
+
+		optVis.id = name + "_asc";
 		optVis.style.backgroundPositionX = `-${48 * rx++}px`;
 		optVis.onclick = sortScenesTrigger;
 		linkDisp.appendChild(optVis);
 	}
-	
+
 	var listorder = orderings[order](scenes);
-	
+
 	if (ascending) listorder = listorder.reverse();
-	
+
 	for (var [i, sid] of Object.entries(listorder)) {
 		const vis = presentLink(sid, scenes[sid], abs);
 		linkDisp.appendChild(vis);
 	}
-	
+
 	var optVis = document.createElement('a');
-	
+
 	optVis.className = "menuOption";
 	optVis.href = "/cc3d/crafter/"
-	
+
 	const optVisTitle = document.createElement('span');
-	
+
 	optVisTitle.innerHTML += '<span class="mode-icon mode-icon-animated"></span> ';
-	
+
 	optVisTitle.innerHTML += "Crafter";
-	
+
 	optVis.appendChild(optVisTitle);
-	
+
 	linkDisp.appendChild(optVis);
 }
 
